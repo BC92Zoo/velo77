434a0520422d080efbbb91d477fb799a50779fdd
SaucePlusUltra <npigeau@myges.fr>
2021-06-13 14:29:14 +0200

    ++

--- frontend/app/src/main/java/com/example/velo77/LoginActivity.java
+++ frontend/app/src/main/java/com/example/velo77/LoginActivity.java
@@ -1,4 +1,28 @@
 package com.example.velo77;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.btnLogin = findViewById(R.id.btn_login);
+        this.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( LoginActivity.this , HomeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
 }
